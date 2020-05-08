8677effcaec540e51705f130c11449bbd2215493
Patrick Faramaz <faramazpat@gmail.com>
2020-05-08 14:33:56 +0200

    complete data podcast general

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,4 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const podcastInfo = require("./src/podcast-infos")
 
 module.exports = {
   /* Your site config here */
@@ -17,29 +13,7 @@ module.exports = {
     `gatsby-plugin-mdx`,
     {
       resolve: `gatsby-plugin-podcast-feed-mdx`,
-      options: {
-        title: `Double Slash`,
-        subtitle: `Podcast sur le développement web`,
-        description: `description.....`,
-        summary: `Le podcast dédié aux outils et aux techniques pour le développement web`,
-        podcastType: `episodic`,
-        siteUrl: `https://www.slash-podcast.fr`,
-        imageUrl: `https://www.slash-podcast.fr/double-slash.png`,
-        feedUrl: `https://www.slash-podcast.fr/pocast-rss-feed.xml`,
-        language: `fr-FR`,
-        copyright: `Copyright © 2020 Some Owner`,
-        authorName: `The Author`,
-        ownerName: `The Owner`,
-        ownerEmail: `owner@podcast.com`,
-        managingEditor: `editor@podcast.com`,
-        webMaster: `support@podcast.com`,
-        explicit: `no`,
-        publicationDate: `April 30, 2020 10:00:00 GMT`,
-        category1: `Technology`,
-        subCategory1: `Books`,
-        timeToLive: `60`,
-        outputPath: `/podcast-rss-feed.xml`,
-      },
+      options: podcastInfo,
     },
   ],
 }
---
+++ src/podcast-infos.js
@@ -0,0 +1,35 @@
+// https://www.feedforall.com/itune-tutorial-tags.htm
+// https://theaudacitytopodcast.com/how-to-start-using-the-new-itunes-podcast-tags-for-ios-11-tap316/
+module.exports = {
+  title: `Double Slash`,
+  // baseline
+  subtitle: `Podcast sur le développement web moderne`,
+  // short description
+  summary: `Le podcast dédié aux outils et aux techniques pour le développement web moderne`,
+  // long description
+  description: `Double Slash, un podcast avec 2 animateurs qui se retrouvent pour discuter des outils et des techniques pour le développement moderne de site web et d’application web.
+  Retrouvez-nous régulièrement pour parler de sujets variés tels que la JAMStack, l’accessibilité, l’écoconception, React JS, Vue JS et des retours d’expériences sur des expériences.
+  Également, des débats autour de sujets polémiques tels que la communication entre développeurs/designers ou comment avoir un discours compréhensible par les clients.
+  Nous serons accompagnés de temps en temps par des invités experts dans leur domaine pour approfondir un sujet et avoir des retours d’expériences.
+  Faites-nous vos retours par mail sur  <![CDATA[
+    <a href="mailto:info@slash-podcast.fr">info@slash-podcast.fr</a>
+  ]]>
+  `,
+  // episodic || serial
+  podcastType: `episodic`,
+  siteUrl: `https://slash-podcast.fr`,
+  imageUrl: `https://slash-podcast.fr/double-slash.png`,
+  feedUrl: `https://slash-podcast.fr/pocast-rss-feed.xml`,
+  language: `fr-FR`,
+  copyright: `Copyright © 2020 Double Slash`,
+  authorName: `Double Slash`,
+  ownerName: `Double Slash`,
+  ownerEmail: `info@slash-podcast.fr`,
+  managingEditor: `info@slash-podcast.fr`,
+  webMaster: `info@slash-podcast.fr`,
+  explicit: `no`,
+  publicationDate: `April 01, 2020 10:00:00 GMT`,
+  category1: `Technology`,
+  timeToLive: `60`,
+  outputPath: `/podcast-rss-feed.xml`,
+}
