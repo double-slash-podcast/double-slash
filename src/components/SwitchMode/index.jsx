ac78c313b1b4b7229dff3805084597241ab1a8e0
Patrick Faramaz <faramazpat@gmail.com>
2020-08-11 11:13:18 +0200

    add icon on switcher

--- src/components/SwitchMode/index.jsx
+++ src/components/SwitchMode/index.jsx
@@ -1,29 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import S from './styles.module.css';
+import SunIcon from './SunIcon';
+import MoonIcon from './MoonIcon';
 
 const SwitchMode = ({name, defaultState, onChange}) => {
   const [state, changeState] = useState(false);
   useEffect(() => changeState(defaultState), [defaultState]);
   return (
-    <label htmlFor={name} className={S.container}>
-      <input
-        id={name}
-        onChange={() => {
-          onChange(!state);
-          changeState(!state);
-        }}
-        checked={state}
-        type="checkbox"
-      />
-      <span
-        className={S.switch}
-        role="switch"
-        aria-label={name}
-        aria-checked={state === true ? 'true' : 'false'}
-      >
-        <span className={S.shadow} />
-      </span>
-    </label>
+    <div className={S.switcher}>
+      <SunIcon />
+      <label htmlFor={name} className={S.container}>
+        <input
+          id={name}
+          onChange={e => {
+            e.preventDefault();
+            onChange(!state);
+            changeState(!state);
+          }}
+          checked={state}
+          type="checkbox"
+        />
+        <span
+          className={S.switch}
+          role="switch"
+          aria-label={name}
+          aria-checked={state === true ? 'true' : 'false'}
+        >
+          <span className={S.shadow} />
+        </span>
+      </label>
+      <MoonIcon />
+    </div>
   );
 };
 
