484a7cc744e4ee5a63d69a3db82145653a4652b9
Patrick Faramaz <faramazpat@gmail.com>
2020-08-24 13:00:46 +0200

    update url for github source podcast

--- src/helpers/git.js
+++ src/helpers/git.js
@@ -1,4 +1,4 @@
-const repo = 'https://github.com/ipatate/double-slash/blob/master';
+const repo = 'https://github.com/double-slash-podcast/double-slash/blob/master';
 
 export const getGitUrl = fileAbsolutePath => {
   return `${repo}${fileAbsolutePath.slice(fileAbsolutePath.indexOf('/src'))}`;
